a1d397218a003b76b647aded67ef6f8ed52612cc
carlos <carlos_sanchez-95@hotmail.com>
2017-08-22 16:20:33 -0500

    se agrego el archivo paginaphp.php a la carpeta proyecto

---
+++ paginaphp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mi pagina PHP</title>
+</head>
+<body style="background-color:#184BE4;">
+<h1>Pagina prueba  con PHP</h1>
+
+<?php
+	
+	$variable = 1;
+	$n1 = "carlos";
+	
+	echo $variable;
+	echo "<br>";
+	echo "Carlos Ulises Sanchez Prado  <br>"; 
+	echo "Materia: Lenguaje de programacion visual  <br>";
+	echo "Dias que se imparte: Martes y miercoles de 7:00 - 8:30  <br>";
+	echo "  <br>";
+	echo "  <br>";
+	echo "  La programación orientada a objetos (POO, u OOP según sus siglas en inglés) 
+	es un paradigma de programación que viene a innovar la forma de obtener resultados. 
+	Los objetos manipulan los datos de entrada para la obtención de datos de salida específicos, 
+	donde cada objeto ofrece una funcionalidad especial.
+	Muchos de los objetos pre-diseñados de los lenguajes de programación actuales permiten la agrupación en bibliotecas o librerías,
+	sin embargo, muchos de estos lenguajes permiten al usuario la creación de sus propias bibliotecas.
+	Está basada en varias técnicas, incluyendo herencia, cohesión, abstracción, polimorfismo, acoplamiento y encapsulamiento.<br>";
+	?>
+</body>
+</html>
+
